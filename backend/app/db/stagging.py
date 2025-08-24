d99bc9c2ddfc9f9ad3bbd0fdaff772f6cb08154d
Pbrosten <phbrosten@gmail.com>
2025-08-24 21:26:18 +0200

    [fix] issue with migration not occuring in temporal order

--- backend/app/db/stagging.py
+++ backend/app/db/stagging.py
@@ -14,7 +14,15 @@ DUMP_INCLUSION_LIST = [
 
 def check_new_heaps():
     dump_path = current_app.config['DUMP_PATH']
-    return [os.path.join(dump_path, heap, "mysql") for heap in os.listdir(dump_path)]
+    heaps = os.listdir(dump_path)
+
+    valid_heaps = [
+        heap for heap in heaps
+        if "_" in heap and heap.split("_")[1].isdigit() and heap.split("_")[2].isdigit()
+    ]
+
+    sorted_heaps = sorted(valid_heaps, key=lambda h: (int(h.split("_")[1]), int(h.split("_")[2])))
+    return [os.path.join(dump_path, heap, "mysql") for heap in sorted_heaps]
 
 def fix_insert_ignore(sql: str) -> str:
     return re.sub(r'^insert\s+ignore', 'insert or ignore', sql, flags=re.IGNORECASE)
