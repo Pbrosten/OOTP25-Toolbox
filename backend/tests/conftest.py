1ff4bebf429965c400332ca84edfc78d755fef65
Pbrosten <phbrosten@gmail.com>
2025-09-01 21:42:08 +0200

    [test] add tests for app.db.stagging.py

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -7,6 +7,9 @@ def app():
     app = Flask(__name__)
     app.register_blueprint(players_bp)
     app.config['DATABASE'] = ':memory:'
+    app.config['DUMP_PATH'] = "/fake/dump/path"  # Set config here or in test
+    with app.app_context():
+        yield app 
     return app
 
 @pytest.fixture
