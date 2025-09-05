4c0a5467c6be52247330f2dffc1cc53e1adb1b7c
Pbrosten <phbrosten@gmail.com>
2025-09-05 18:52:14 +0200

    [chore] remove unnecessary line in long migation

--- backend/app/db/update.py
+++ backend/app/db/update.py
@@ -95,7 +95,6 @@ def run_migration_short(heap_date, db):
 def run_migration_long(heap_date, db):
     with current_app.open_resource(os.path.join('db','sql_scripts','migration_long.sql'), 'r') as f:
         sql_script = inject_db_path(f.read(), current_app.config["STAGGING"])
-        sql_script = inject_heap_date(sql_script, heap_date)
         db.executescript(sql_script)
 
 def fetch_projection_inputs(heap_date, db):
