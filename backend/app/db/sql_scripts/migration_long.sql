1dc24ef5f46abb7ae8f1389fdc86f017e1dfefcd
Pbrosten <phbrosten@gmail.com>
2025-09-05 18:50:16 +0200

    [refactor] move player table migration from short to long migration

--- backend/app/db/sql_scripts/migration_long.sql
+++ backend/app/db/sql_scripts/migration_long.sql
@@ -1,5 +1,23 @@
 ATTACH DATABASE '{{STAGGING_DB_PATH}}' AS stage;
 
+INSERT OR IGNORE INTO players(player_id, first_name, last_name, birth_date,
+position, height, weight, bats, throws, free_agent, team_id, prone_overall)
+SELECT
+    player_id,
+    first_name,
+    last_name,
+    date_of_birth,
+    position,
+    height,
+    weight,
+    bats,
+    throws,
+    free_agent,
+    team_id,
+    prone_overall
+FROM stage.players
+WHERE retired = 0;
+
 INSERT OR IGNORE INTO teams(team_id, name, abbr, nickname, division_id,
 league_id, human_team, background_color, text_color)
 SELECT
