515f4c42884985546a308f8798968ed24eb946b7
Pbrosten <phbrosten@gmail.com>
2025-09-09 23:56:10 +0200

    [feat] add api/players/<player_id>/career/batting endpoint

--- backend/app/api/players.py
+++ backend/app/api/players.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 from flask import Blueprint, jsonify, current_app
 from app.db.connection import get_db, close_db
@@ -84,4 +85,31 @@ def get_player_details_by_id(player_id):
                 return jsonify({"error": "Player not found"}), 404
     finally:
         close_db()
-        
+
+@bp.route('/<int:player_id>/career/batting', methods=['GET'])
+def get_player_career_batting(player_id):
+    """
+    Retrieve the player career batting statistics for a single player by their unique ID.
+
+    Args:
+        player_id (int): The ID of the player to retrieve.
+
+    Returns:
+        JSON response:
+            - Player career batting records if found.
+            - 404 error if not found.
+    """
+    con = get_db()
+    try: 
+        with current_app.open_resource(os.path.join('db','sql_scripts','api','get_player_career_batting.sql'), 'r') as f:
+            cursor = con.execute(f.read(), (player_id,))
+            rows = cursor.fetchall()
+            if rows:
+                # process pulled data here.
+                df = pd.DataFrame([dict(row) for row in rows])
+                print(df.groupby(['year', 'abbr']).sum().reset_index().to_dict(orient='records'))
+                return jsonify(df.groupby(['year', 'abbr']).sum().reset_index().to_dict(orient='records'))
+            else:
+                return jsonify({'error': 'Player not found'}), 404
+    finally:
+        close_db()
