8a5a97e31e96db39a93ecc312d73b62f9b3661b4
Pbrosten <phbrosten@gmail.com>
2025-09-12 15:01:16 +0200

    [feat] placeholder for PlayerDetails component

---
+++ frontend/src/components/PlayerDetails.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ player_id: number}>()
+</script>
+
+<template>
+    <a id="player-picture">
+      <img src="/baseball.png" class="logo" alt="Player picture" />
+    </a>
+    <h1>Player Name {{ player_id }}</h1>
+    <span>
+        <p>Player Position | Bats/Throws: R/R | Height Weight | Age: ##</p>
+    </span>
+    <div>
+        <ul>
+            <li>batting stats year -3</li>
+            <li>batting stats year -2</li>
+            <li>batting stats year -1</li>
+            <li>batting career stats</li>
+        </ul>
+    </div>
+</template>
