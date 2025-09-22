2dba610ff9bb393b719c584ed18d5639148808e5
Pbrosten <phbrosten@gmail.com>
2025-09-22 08:54:20 +0200

    [feat] add Percentiles to PlayerProfile and watch playerDetails

--- frontend/src/views/PlayerProfile.vue
+++ frontend/src/views/PlayerProfile.vue
@@ -2,7 +2,27 @@
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import PlayerDetails from '@/components/PlayerDetails.vue'
+import BatterPercentiles from '@/components/percentiles/BatterPercentiles'
 
 const route = useRoute()
 const playerId = Number(route.params.id)
+
+const playerDetailsRef = ref<InstanceType<typeof PlayerDetails> | null>(null)
+
+watch(
+  () => playerDetailsRef.value?.playerDetails,
+  (details) => {
+    if (details?.position === 'P') {
+      console.log('Pitcher detected in parent view!')
+    }
+  }
+)
 </script>
+
+<template>
+  <div>
+    <PlayerDetails ref="playerDetailsRef" :playerId="playerId" />
+    <PitcherPercentiles v-if="playerDetailsRef.position === 'P'" :playerId="playerId"/>
+    <BatterPercentiles v-else :playerId="playerId"/>
+  </div>
+</template>
