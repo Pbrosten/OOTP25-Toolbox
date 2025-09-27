ef66b62f91358c0ce7c4e0faf4549e506973fc3a
Pbrosten <phbrosten@gmail.com>
2025-09-27 22:12:48 +0200

    [feat] add rounding to get_player_expected_batting_percentiles.sql

--- backend/app/db/sql_scripts/api/get_player_expected_batting_percentiles.sql
+++ backend/app/db/sql_scripts/api/get_player_expected_batting_percentiles.sql
@@ -31,54 +31,94 @@ ratings_filtered AS (
 SELECT
 
   -- Projection based percentiles
-  (
-    SELECT COUNT(*) * 1.0
-    FROM expected_filtered
-    WHERE AVG < target_exp.AVG AND AVG IS NOT NULL
-  ) / (SELECT COUNT(*) FROM expected_filtered WHERE AVG IS NOT NULL) AS xBA,
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM expected_filtered
+      WHERE AVG < target_exp.AVG AND AVG IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM expected_filtered WHERE AVG IS NOT NULL
+    )
+   ) AS xBA,
 
-  (
-    SELECT COUNT(*) * 1.0
-    FROM expected_filtered
-    WHERE SLG < target_exp.SLG AND SLG IS NOT NULL
-  ) / (SELECT COUNT(*) FROM expected_filtered WHERE SLG IS NOT NULL) AS xSLG,
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM expected_filtered
+      WHERE SLG < target_exp.SLG AND SLG IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM expected_filtered WHERE SLG IS NOT NULL
+    )
+   ) AS xSLG,
 
-  (
-    SELECT COUNT(*) * 1.0
-    FROM expected_filtered
-    WHERE wOBA < target_exp.wOBA AND wOBA IS NOT NULL
-  ) / (SELECT COUNT(*) FROM expected_filtered WHERE wOBA IS NOT NULL) AS xWOBA,
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM expected_filtered
+      WHERE wOBA < target_exp.wOBA AND wOBA IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM expected_filtered WHERE wOBA IS NOT NULL
+    )
+   ) AS xWOBA,
 
   -- Rating based percentiles
-  (
-    SELECT COUNT(*) * 1.0
-    FROM ratings_filtered
-    WHERE babip < target_rate.gap AND gap IS NOT NULL
-  ) / (SELECT COUNT(*) FROM ratings_filtered WHERE babip IS NOT NULL) AS xBABIP,
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM ratings_filtered
+      WHERE babip < target_rate.gap AND gap IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM ratings_filtered WHERE babip IS NOT NULL
+    )
+   ) AS xBABIP,
 
-  (
-    SELECT COUNT(*) * 1.0
-    FROM ratings_filtered
-    WHERE gap < target_rate.gap AND gap IS NOT NULL
-  ) / (SELECT COUNT(*) FROM ratings_filtered WHERE gap IS NOT NULL) AS barrel_rate,
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM ratings_filtered
+      WHERE gap < target_rate.gap AND gap IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM ratings_filtered WHERE gap IS NOT NULL
+    )
+   ) AS barrel_rate,
 
-  (
-    SELECT COUNT(*) * 1.0
-    FROM ratings_filtered
-    WHERE power < target_rate.gap AND power IS NOT NULL
-  ) / (SELECT COUNT(*) FROM ratings_filtered WHERE gap IS NOT NULL) AS swing_speed,
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM ratings_filtered
+      WHERE power < target_rate.gap AND power IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM ratings_filtered WHERE gap IS NOT NULL
+    )
+   ) AS swing_speed,
 
-  (
-    SELECT COUNT(*) * 1.0
-    FROM ratings_filtered
-    WHERE eye < target_rate.gap AND eye IS NOT NULL
-  ) / (SELECT COUNT(*) FROM ratings_filtered WHERE gap IS NOT NULL) AS chase_rate,
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM ratings_filtered
+      WHERE eye < target_rate.gap AND eye IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM ratings_filtered WHERE gap IS NOT NULL
+    )
+   ) AS chase_rate,
 
-  (
-    SELECT COUNT(*) * 1.0
-    FROM ratings_filtered
-    WHERE strikeouts < target_rate.gap AND strikeouts IS NOT NULL
-  ) / (SELECT COUNT(*) FROM ratings_filtered WHERE gap IS NOT NULL) AS whiff_rate
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM ratings_filtered
+      WHERE strikeouts < target_rate.gap AND strikeouts IS NOT NULL
+    ) * 100 / 
+    (
+      SELECT COUNT(*) FROM ratings_filtered WHERE gap IS NOT NULL
+    )
+   ) AS whiff_rate
 
 FROM players_batting_expected AS target_exp
 JOIN players_batting AS target_rate ON target_exp.rating_id = target_rate.rating_id
