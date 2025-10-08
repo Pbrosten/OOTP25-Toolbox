b54fee7517dfdd8087a20d2645319e9da282e49c
Pbrosten <phbrosten@gmail.com>
2025-10-08 23:18:59 +0200

    [chore] remove center of PlayerDetails

--- frontend/src/views/PlayerProfile.vue
+++ frontend/src/views/PlayerProfile.vue
@@ -20,7 +20,7 @@ const PitcherPercentiles = {}
 <template>
   <div class="max-w-[1400px] mx-auto p-4">
     <div
-      class="flex gap-4 items-center
+      class="flex gap-4 items-start
              md:flex-row flex-col"
     >
       <PlayerDetails
