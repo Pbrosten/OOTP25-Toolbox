c1036ec02b55a93d941aac43190eca457768c14f
Pbrosten <phbrosten@gmail.com>
2025-10-09 10:39:30 +0200

    [feat][backend] add player expected value sql script

---
+++ backend/app/db/sql_scripts/api/get_player_expected_value_percentiles.sql
@@ -0,0 +1,73 @@
+WITH target_player AS (
+  SELECT r.rating_id, r.rating_date, r.league_id
+  FROM players_rating r
+  WHERE r.rating_id = :rating_id
+),
+
+-- Expected Stats Comparison Group
+expected_filtered AS (
+  SELECT rv.*
+  FROM players_run_value AS rv
+  JOIN players_rating AS r ON rv.rating_id = r.rating_id
+  JOIN players AS p ON r.player_id = p.player_id
+  JOIN target_player AS t 
+    ON r.rating_date = t.rating_date
+    AND (
+        CASE 
+          WHEN :is_mlb THEN r.league_id = 203
+          ELSE r.league_id = t.league_id
+        END
+   )
+  WHERE p.position != 'P'
+)
+
+
+SELECT
+  -- Projection based percentiles
+  ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM expected_filtered
+      WHERE batting_runs < target_exp.batting_runs AND batting_runs IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM expected_filtered WHERE batting_runs IS NOT NULL
+    )
+   ) AS batting_runs_percentile,
+
+   ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM expected_filtered
+      WHERE basepath_runs < target_exp.basepath_runs AND basepath_runs IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM expected_filtered WHERE basepath_runs IS NOT NULL
+    )
+   ) AS basepath_runs_percentile,
+
+   ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM expected_filtered
+      WHERE fielding_runs < target_exp.fielding_runs AND fielding_runs IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM expected_filtered WHERE fielding_runs IS NOT NULL
+    )
+   ) AS fielding_runs_percentile,
+
+   ROUND(
+    (
+      SELECT COUNT(*) * 1.0
+      FROM expected_filtered
+      WHERE total_runs < target_exp.total_runs AND total_runs IS NOT NULL
+    ) * 100  /
+    (
+      SELECT COUNT(*) FROM expected_filtered WHERE total_runs IS NOT NULL
+    )
+   ) AS total_runs_percentile
+
+
+FROM players_run_value AS target_exp
+WHERE target_exp.rating_id = :rating_id;
