415f0540afb9e180dfcd284ef1aaf98c63d11c7c
Pbrosten <phbrosten@gmail.com>
2025-10-09 16:56:47 +0200

    [chore][backend] update DUMP_DIR env in DUMP_PATH definition

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ load_dotenv(path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
-    DUMP_PATH = path.join(environ.get('GAME_PATH',''), f'{environ.get("GAME_STATE",'')}', '{environ.get("DUMP_DIR",'')}')
+    DUMP_PATH = path.join(environ.get('GAME_PATH',''), f'{environ.get("GAME_STATE",'')}', f'{environ.get("DUMP_DIR",'')}')
 
 class ProdConfig(Config):
     FLASK_ENV = 'production'
