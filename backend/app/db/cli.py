dd5d457e7cc2644f48caaf45387d49d61b6365bf
Pbrosten <phbrosten@gmail.com>
2025-11-01 23:04:29 +0100

    [feat] update db connection to support mysql

--- backend/app/db/cli.py
+++ backend/app/db/cli.py
@@ -8,17 +8,26 @@ from .staging import check_new_heaps
 from .update import process_single_heap
 
 
-@click.command('init-db')
+@click.command("init-db")
 def init_db_command():
-    db = get_db()
-    with current_app.open_resource(os.path.join('db','sql_scripts','schema.sql'), 'r') as f:
-        db.executescript(f.read())
-    click.echo('Initialized the SQLite database.')
+    conn = get_db()
+    cursor = conn.cursor()
+    with current_app.open_resource(
+        os.path.join("db", "sql_scripts", "schema-maria.sql"), "r", encoding="utf-8"
+    ) as f:
+        sql_script = f.read()
+        for statement in sql_script.strip().split(";"):
+            if statement.strip():
+                cursor.execute(statement)
+    click.echo("Initialized the SQLite database.")
+    cursor.close()
 
-@click.command('update-db')
+
+@click.command("update-db")
 def update_db_command():
     update_db()
-    click.echo('Updated the SQLite database.')
+    click.echo("Updated the SQLite database.")
+
 
 def update_db():
     logger = current_app.logger
@@ -32,12 +41,16 @@ def update_db():
     count_short = sum(1 for _, is_short in sorted_heaps if is_short)
     count_long = total_heaps - count_short
 
-    logger.info(f"Found {count_long} new long heap(s) and {count_short} new short heap(s).")
-    db = get_db()
+    logger.info(
+        f"Found {count_long} new long heap(s) and {count_short} new short heap(s)."
+    )
+    conn = get_db()
 
     logger.info("Migrating heaps in order")
     for heap_number, (heap_path, short_heap_flag) in enumerate(sorted_heaps, 1):
-        process_single_heap(heap_path, heap_number, total_heaps, db, short_heap=short_heap_flag)
+        process_single_heap(
+            heap_path, heap_number, total_heaps, conn, short_heap=short_heap_flag
+        )
 
     logger.info("Migration and projection complete!")
     close_db()
