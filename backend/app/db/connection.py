99fda58a7ea2a98005f0a934c4ecea2edb997727
Pbrosten <phbrosten@gmail.com>
2025-11-01 23:21:29 +0100

    [feat] streamline app.db.connection get_db function

--- backend/app/db/connection.py
+++ backend/app/db/connection.py
@@ -8,27 +8,24 @@ from pymysql.cursors import DictCursor
 logger = logging.getLogger("app.db.connection")
 
 
-def get_db():
+def get_db() -> pymysql.connections.Connection:
     if "db" not in g:
         cfg = current_app.config
-        if cfg.get("DB_HOST") and pymysql is not None:
-            logger.info("Found MariaDB")
-            g.db = pymysql.connect(
-                host=cfg.get("DB_HOST"),
-                user=cfg.get("DB_USER"),
-                password=cfg.get("DB_PASSWORD"),
-                db=cfg.get("DB_NAME"),
-                port=int(cfg.get("DB_PORT", 3306)),
-                cursorclass=DictCursor,
-                charset="utf8mb4",
-                autocommit=False,
-            )  # type: ignore
-        else:
-            logger.info("defaulting to SQLite")
-            g.db = sqlite3.connect(
-                cfg["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
-            )
-            g.db.row_factory = sqlite3.Row
+
+        host = cfg.get("DB_HOST")
+        if not host:
+            logger.error("Missing database host in configuration!")
+            raise RuntimeError("Database host not configured")
+
+        g.db = pymysql.connect(
+            host=host,
+            user=cfg.get("DB_USER"),
+            password=cfg.get("DB_PASSWORD"),
+            db=cfg.get("DB_NAME"),
+            port=int(cfg.get("DB_PORT", 3306)),
+            charset="utf8mb4",
+            autocommit=False,
+        )
 
     return g.db
 
